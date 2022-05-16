c505e2e99958695bbd4050862b2d5d88094d1e08
caritoespicaita <caritoespicaita@hotmail.com>
2022-05-16 14:21:14 -0500

    update task 9

--- models/review.py
+++ models/review.py
@@ -1,10 +1,17 @@
 #!/usr/bin/python3
 """ Review module for the HBNB project """
 from models.base_model import BaseModel
+from models.base_model import Base
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
 
 
-class Review(BaseModel):
-    """ Review classto store review information """
-    place_id = ""
-    user_id = ""
-    text = ""
+class Review(BaseModel, Base):
+    """ Review classto store review information
+        Inherits from SQLAlchemy Base and links to the MySQL table reviews.
+    """
+    __tablename__ = "reviews"
+    if os.environ.get('HBNB_TYPE_STORAGE') == 'db':
+        place_id = Column(String(60), ForeignKey("places.id"), nullable=False)
+        user_id = Column(String(60), ForeignKey("user.id"), nullable=False, )
+        text = Column(String(1024), nullable=False)
