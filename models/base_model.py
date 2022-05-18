0bd660971a635d8b93fac7cd55bb04892ab6067b
Rcastro89 <r_castro_m@yahoo.es>
2022-05-17 23:32:48 -0500

    prueba 10

--- models/base_model.py
+++ models/base_model.py
@@ -6,6 +6,7 @@ import uuid
 from datetime import datetime
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, String, DateTime
+import os
 
 Base = declarative_base()
 
@@ -27,6 +28,10 @@ class BaseModel():
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
         else:
+            if os.environ.get('HBNB_TYPE_STORAGE') == 'db':
+                self.id = str(uuid.uuid4())
+                self.created_at = datetime.now()
+                self.updated_at = datetime.now()
             kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
                                                      '%Y-%m-%dT%H:%M:%S.%f')
             kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
