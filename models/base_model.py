690792d9b58fd8df833f3b1718606eae508cfd29
Rcastro89 <r_castro_m@yahoo.es>
2022-05-17 23:45:49 -0500

    10 final

--- models/base_model.py
+++ models/base_model.py
@@ -6,6 +6,7 @@ import uuid
 from datetime import datetime
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, String, DateTime
+import os
 
 Base = declarative_base()
 
@@ -27,11 +28,18 @@ class BaseModel():
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
         else:
-            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            del kwargs['__class__']
+            if os.environ.get('HBNB_TYPE_STORAGE') == 'db':
+                self.id = str(uuid.uuid4())
+                self.created_at = datetime.now()
+                self.updated_at = datetime.now()
+                kwargs['updated_at'] = self.updated_at
+                kwargs['created_at'] = self.created_at
+            else:
+                kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
+                                                        '%Y-%m-%dT%H:%M:%S.%f')
+                kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
+                                                        '%Y-%m-%dT%H:%M:%S.%f')
+                del kwargs['__class__']
             self.__dict__.update(kwargs)
 
     def __str__(self):
