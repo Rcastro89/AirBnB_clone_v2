2865dffcbc2ecfa8fb3651cc682986eb79d26a49
Rcastro89 <r_castro_m@yahoo.es>
2022-06-01 23:53:50 -0500

    Flask2

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -6,17 +6,19 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/', strict_slashes=False)
-def hello():
-    """function to return message"""
+@app.route("/", strict_slashes=False)
+def hello_world():
+    """function to return message
+    """
     return "Hello HBNB!"
 
 
-@app.route('/hbnb', strict_slashes=False)
-def hello_1():
-    """function to return message"""
-    return "HBNB!"
+@app.route("/hbnb", strict_slashes=False)
+def route_hbnb():
+    """function to return message
+    """
+    return "HBNB"
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000')
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
